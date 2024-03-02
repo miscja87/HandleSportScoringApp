762e9ce89df56e598a7d6e9dfba96bb4351afa04
miscja87 <miscja@hotmail.com>
2024-03-02 08:52:25 +0100

    Add icons

--- App.js
+++ App.js
@@ -383,8 +383,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#252830',
-    padding: 2
+    backgroundColor: '#252830'
   },
   empty: {
     flex: 1
